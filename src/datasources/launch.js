09e1efa3883f044b3b5335d633a6dbca28bdc47f
jeroennknol <jeroen-knol@live.nl>
2019-01-04 15:59:51 +0100

    Initial commit

---
+++ src/datasources/launch.js
@@ -0,0 +1,46 @@
+const { RESTDataSource } = require('apollo-datasource-rest');
+
+class LaunchAPI extends RESTDataSource {
+  constructor() {
+    super();
+    this.baseURL = 'https://api.spacexdata.com/v2/';
+  }
+
+  async getAllLaunches() {
+    const res = await this.get('launches');
+    return res && res.length ? res.map(l => this.launchReducer(l)) : [];
+  }
+
+  async getLaunchById({ launchId }) {
+    const res = await this.get('launches', { flight_number: launchId });
+    return this.launchReducer(res[0]);
+  }
+
+  async getLaunchesByIds({ launchIds }) {
+    return Promise.all(
+      launchIds.map(launchId => this.getLaunchById({ launchId }))
+    );
+  }
+
+  /* eslint-disable class-methods-use-this */
+  launchReducer(launch) {
+    return {
+      id: launch.flight_number || 0,
+      cursor: `${launch.launch_date_unix}`,
+      site: launch.launch_site && launch.launch_site.site_name,
+      mission: {
+        name: launch.mission_name,
+        missionPatchSmall: launch.links.mission_patch_small,
+        missionPatch: launch.links.mission_patch,
+      },
+      rocket: {
+        id: launch.rocket.rocket_id,
+        name: launch.rocket.rocket_name,
+        type: launch.rocket.rocket_type,
+      },
+    };
+  }
+  /* eslint-enable class-methods-use-this */
+}
+
+module.exports = LaunchAPI;
